46227f10e938d93790b35e6c80395968f7835c28
Muhammet Fatih EREN <muhammet.fatih.eren@gmail.com>
2019-08-01 13:49:15 +0300

    uploading files into git

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from datetime import datetime
+from django.core.paginator import Paginator
+from.models import Post
+
+class BlogPageView(ListView):
+	model = Post
+	template_name='index.html'
+	paginate_by = 3
+
+class BlogDetailView(DetailView):
+	model = Post
+	template_name='post.html'
